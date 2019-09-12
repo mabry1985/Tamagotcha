5a5cfee20bef38cfe4cfa27537724ccbb8978c7f
Student <example@example.com>
2019-09-12 15:42:59 -0700

    bunch more css styling

--- src/components/Tamagotcha.jsx
+++ src/components/Tamagotcha.jsx
@@ -3,6 +3,8 @@ import { CSSTransitionGroup } from 'react-transition-group';
 import background from '../assets/images/background.png';
 import tv from '../assets/videos/channelchanging.mp4';
 import grumpycat from '../assets/images/grumpy-cat.png';
+import border from '../assets/images/Rectangle.png';
+import border1 from '../assets/images/Rectangle.png';
 
 class Tamagotcha extends Component {
   constructor(props) {
@@ -11,12 +13,14 @@ class Tamagotcha extends Component {
       health: 5,
       hunger: 0,
 
+      hungerTimerCheck: true,
+
       // sleepiness: 0,
       // boredom: 0,
     };
   }
 
-  componentDidMount() {
+  componentDidMount = () => {
     this.hungerTimer = setInterval(
       () => this.hungerMeter(),
       1000
@@ -31,38 +35,45 @@ class Tamagotcha extends Component {
       () => this.healthCheck(),
       1000
     );
-  }
+  };
 
-  hungerCheck() {
-    if (this.state.hunger >= 5) {
+  hungerCheck = () => {
+    if (this.state.hunger == 5) {
       clearInterval(this.hungerTimer);
       this.setState({
         health: this.state.health -= 1,
       });
     }
-  }
+  };
 
-  hungerMeter() {
+  hungerMeter = () => {
     this.setState({
       hunger: this.state.hunger += 1,
     });
-  }
+  };
 
   heal = () =>
     this.setState({
       health: this.state.health = 5,
     });
 
-  healthCheck() {
+  healthCheck = () => {
     if (this.state.health < 1) {
       this.setState({
         health: this.state.health = 0,
       });
     }
-  }
+  };
 
   feed = () => {
     if (this.state.hunger > 1) {
+      if (this.state.hungerTimerCheck == true) {
+        clearInterval(this.hungerTimer);
+        this.hungerTimer;
+      } else {
+        this.hungerTimer;
+      }
+
       this.setState({
         hunger: this.state.hunger -= 2,
       });
@@ -82,20 +93,21 @@ class Tamagotcha extends Component {
         transitionEnter={false}
         transitionLeave={false}
       >
-        <img src={background} className='background' />
-        <img src={grumpycat} className='grumpy-cat' />
-      <video autoPlay muted loop>
-          <source src={tv} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <h1>Name: {this.props.name}</h1>
-        <h2>Health: {this.state.health}</h2>
-        <h2>Hunger: {this.state.hunger}</h2>
-        {/* <h2>Sleepiness: {this.state.sleepiness}</h2>
-           <h2>Boredom: {this.state.boredom}</h2>
-        */}
-        <button className='btn btn-success' onClick={this.feed}>feed</button>
-        <button className='btn btn-success' onClick={this.heal}>heal</button>
+          <img src={background} className='background' />
+          <img src={grumpycat} className='grumpy-cat' />
+          <img src={border} className='border' />
+          <video autoPlay muted loop>
+            <source src={tv} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          <h1>Name: {this.props.name}</h1>
+          <h2>Health: {this.state.health}</h2>
+          <h2>Hunger: {this.state.hunger}</h2>
+          {/* <h2>Sleepiness: {this.state.sleepiness}</h2>
+             <h2>Boredom: {this.state.boredom}</h2>
+          */}
+          <button className='btn btn-success' onClick={this.feed}>feed</button>
+          <button className='btn btn-success' onClick={this.heal}>heal</button>
       </CSSTransitionGroup>
 
     );
